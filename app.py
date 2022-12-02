cf75860a46afc9cff738473003b50b8d016faafa
sooraj14 <chowkisooraj@gmail.com>
2022-12-02 09:38:43 +0530

    modifications in UI

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def Home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

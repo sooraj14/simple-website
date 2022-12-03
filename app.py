6b83b92c24b2a48a90532e5c74e4edc691190319
Shodhan Shetty <Shodhanshetty144@gmail.com>
2022-12-03 23:11:09 +0530

    Added routes and changed some Html templates

--- app.py
+++ app.py
@@ -9,5 +9,15 @@ app = Flask(__name__)
 def Home():
     return render_template('home.html')
 
+
+@app.route('/about')
+def About():
+    return render_template('aboutus.html')
+
+
+@app.route('/contact')
+def Contact():
+    return render_template('contactus.html')
+
 if __name__ == '__main__':
     app.run(debug=True)

63be0c12ab12f0bfdbb3284ddd8b037633f6c4fc
Shodhan Shetty <Shodhanshetty144@gmail.com>
2023-05-10 13:34:19 +0530

    data collection from contacts page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -15,8 +15,19 @@ def About():
     return render_template('aboutus.html')
 
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def Contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        # print("hello", name)
+        email = request.form['email']
+        gender = request.form['gender']
+        password = request.form['password']
+        pa = ''
+        for i in range(len(password)):
+            pa = pa + '*'
+        # print( email ,gender, password)
+        return f"Ok your contacts details have been saved checkk the details below\nname:{name}, email:{email}, gender:{gender}, password:{pa}"
     return render_template('contactus.html')
 
 if __name__ == '__main__':
